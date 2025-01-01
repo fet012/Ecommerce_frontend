755973da1bc03f9452f742429935ff83dc036e11
fet012 <falolachristiana446@gmail.com>
2025-01-01 17:46:17 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,4 +8,20 @@ export default defineConfig({
   server: {
     port: 3000, 
   },
-})
+  build: {
+    outDir: 'public', // or 'build', 'dist'
+    index: 'index.html',
+    chunkSizeWarningLimit: 1000, // Increase limit (optional)
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Customize chunking logic
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+
+});
